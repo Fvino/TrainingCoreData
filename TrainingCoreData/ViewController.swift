e9d45037fabbf919c0b2ea8fc1b9e67e08b8b37b
ValeriyFomin <fvg2@icloud.com>
2022-02-25 10:19:55 +0100

    Initial Commit

---
+++ TrainingCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrainingCoreData
+//
+//  Created by Valeriy Fomin on 25/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
