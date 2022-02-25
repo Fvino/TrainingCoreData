209b3901103d9d7dc5455f6d81ddf8fbee8e3c2e
ValeriyFomin <fvg2@icloud.com>
2022-02-25 14:25:01 +0100

    add NavigationController, setup navigationBar

--- TrainingCoreData/ViewController.swift
+++ TrainingCoreData/ViewController.swift
@@ -11,9 +11,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
+        view.backgroundColor = .white
+
+        navigationItem.title = "Companies"
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image:  UIImage(systemName: "plus.square")?.withTintColor(.white, renderingMode: .alwaysOriginal), style: .done, target: self, action: #selector(handleAddCompany))
+
+
+        setupNavigationStyle()
+
     }
 
+    //MARK: - Methods
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
+    func setupNavigationStyle() {
+
+        navigationController?.navigationBar.isTranslucent = false
+        let lightRed = UIColor(red: 247/255, green: 66/255 , blue: 82/255, alpha: 1)
+        navigationController?.navigationBar.backgroundColor = lightRed
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    }
+
+    @objc func handleAddCompany() {
+        print("Adding company...")
+    }
 }
 
